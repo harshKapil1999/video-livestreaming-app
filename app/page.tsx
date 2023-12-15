d4ef565097bf3a450ece0dafff54b497dc1a1082
Harsh Kapil <kapilharsh1999@gmail.com>
2023-12-15 22:49:15 +0530

    Clerk Authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
-    <>
-    <h1 className="text-2xl font-bold">Hello Live Streaming App</h1>
-    <Button size="lg" variant="destructive" >Click me</Button>
-    </>
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/"/>
+      <p>Only authenticated users can see this</p>
+    </div>
     
   )
 }
