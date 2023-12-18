7f70bfdd1c611eee197cd1b59dedc063d768b8af
Harsh Kapil <kapilharsh1999@gmail.com>
2023-12-18 23:42:02 +0530

    implimented live badge and avatar in sidebar

--- app/(browse)/_components/sidebar/recommended.tsx
+++ app/(browse)/_components/sidebar/recommended.tsx
@@ -2,6 +2,7 @@
 
 import { useSidebar } from "@/store/use-sidebar";
 import { User } from "@prisma/client"
+import { UserItem, UserItemSkeleton } from "./user-item";
 
 interface RecommendedProps {
     data: User[];
@@ -22,6 +23,27 @@ export const Recommended = ({
                     </p>
                 </div>
             )}
+            <ul>
+                {data.map((user) => (
+                    <UserItem 
+                        key={user.id}
+                        username={user.username}
+                        imageUrl={user.imageUrl}
+                        isLive={true}
+                        
+                    />
+                ))}
+            </ul>
         </div>
-    )
-}
+    );
+};
+
+export const RecommendedSkeleton = () => {
+    return (
+        <ul className="px-2">
+            {[...Array(3)].map((_, i) => (
+                <UserItemSkeleton key={i} />
+            ))}
+        </ul>
+    );
+};
