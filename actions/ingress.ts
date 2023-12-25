00709785bf4437b1a6fb1046fe3e9237813d21fa
Harsh Kapil <kapilharsh1999@gmail.com>
2023-12-25 23:56:53 +0530

    live streaming models and chat options. layout of the app sidebar, navbar

---
+++ actions/ingress.ts
@@ -0,0 +1,50 @@
+"use server";
+
+import { 
+    IngressAudioEncodingPreset,
+    IngressInput,
+    IngressClient,
+    IngressVideoEncodingPreset,
+    RoomServiceClient,
+   type CreateIngressOptions,
+   
+} from "livekit-server-sdk";
+
+import { TrackSource } from "livekit-server-sdk/dist/proto/livekit_models";
+
+import { db } from "@/lib/db";
+import { getSelf } from "@/lib/auth-service";
+
+const roomService = new RoomServiceClient(
+    process.env.LIVEKIT_API_URL!,
+    process.env.LIVEKIT_API_KEY!,
+    process.env.LIVEKIT_API_SECRET!
+);
+
+//const IngressClient = new IngressClient
+
+export const creatIngress = async (ingressType: IngressInput) => {
+    const self = await getSelf();
+
+    //TODO: Reset previous ingress
+
+    const options: CreateIngressOptions = {
+        name: self.username,
+        roomName: self.id,
+        participantName: self.username,
+        participantIdentity: self.id,
+    };
+
+    if (ingressType === IngressInput.WHIP_INPUT) {
+        options.bypassTranscoding = true;
+    } else {
+        options.video = {
+            source: TrackSource.CAMERA,
+            preset: IngressVideoEncodingPreset.H264_1080P_30FPS_3_LAYERS,
+        };
+        options.audio = {
+            source: TrackSource.MICROPHONE,
+            preset: IngressAudioEncodingPreset.OPUS_STEREO_96KBPS
+        };
+    };
+}
