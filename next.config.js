9acc3432ff0b812bfe84c0abcbfdb7e8fe788322
Harsh Kapil <kapilharsh1999@gmail.com>
2023-12-31 18:52:51 +0530

    Custom error and not found components

--- next.config.js
+++ next.config.js
@@ -8,13 +8,15 @@ const nextConfig = {
             },
           ],
     },
-    /* webpack: (config) => {
+    webpack: (config) => {
         config.module.rules.push({
-            test: /\.mjs$/,
-            include: /node_modules/,
-            type: "javascript/auto",
+          test: /\.mjs$/,
+          include: /node_modules/,
+          type: "javascript/auto",
         });
-    } */
+    
+        return config;
+      },
 }
 
 module.exports = nextConfig
